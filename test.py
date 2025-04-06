5e08059d6b5d991241554415afba299161951bdb
baylee.wang <bayleerong@gmail.com>
2025-04-06 01:41:36 -0400

    first time try

---
+++ test.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import math
+import pdb
+
+# from test2 import my_fun
+import test2 as t2
+
+t2.my_fun_t2()
+t2.my_fun_t1()
+
+# dtype = torch.float
+# device = torch.device("cpu")
+# # device = torch.device("cuda:0") # Uncomment this to run on GPU
+
+# # Create random input and output data
+# xx = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
+# yy = torch.sin(xx)
+
+# # Randomly initialize weights
+# aa = torch.randn((), device=device, dtype=dtype)
+# bb = torch.randn((), device=device, dtype=dtype)
+
+# pdb.set_trace()
+
+
+# cc = torch.randn((), device=device, dtype=dtype)
+
+
+# pdb.set_trace()
+# dc = torch.randn((), device=device, dtype=dtype)
+
+# print('aa is:', aa)
+
+# pdb.set_trace()
---
+++ test2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pdb
+
+def my_fun_t1():
+    print("do nothing t1!")
+
+
+def my_fun_t2():
+    print("do nothing t2!")
+
+
+if __name__ == "__main__":
+
+    aaa  = np.arange(15).reshape(3, 5)
+
+    pdb.set_trace() 
